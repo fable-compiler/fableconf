b45e0f0c2aba02f15f87044fdccc90d519a5f483
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-07-06 18:19:03 +0200

    Add sponsor

--- src/Home/View.fs
+++ src/Home/View.fs
@@ -16,17 +16,30 @@ let root model dispatch =
       Icon.icon [Icon.isMedium] [i [ClassName "fa fa-map-marker"] []]
       str " Bordeaux, France"
     ]
-    Image.image [] [img [Src "img/fableconf.png"]]
+    Image.image [] [
+      img [Src "img/fableconf.png"]
+      figcaption [ClassName "has-text-centered"] [em [] [str "FableConf logo by "; a [Href "http://paulbacchus.com/"] [str "Paul Bacchus"]]]
+    ]
     div [ClassName "content"] [
+      br []
       p [] [str "Come to the beautiful city of Bordeaux and be part of the first conference focused on development for the Javascript platform in F# with Fable! Fableconf will be a full day of talks by some of the main contributors to the Fable ecosystem to learn all the new possibilities the Fable compiler opens to F# programmers. And it will also be the first opportunity for the growing Fable community to meet in person, share experiences and plan together the next big project to disrupt the web."]
       p [] [str "No matter if you are new to Fable, don't have much experience in web development or even if you don't know F# yet, if you are a developer interested in writing user interfaces in a functional programming language designed for high productivity and with cutting-edge tooling, Fableconf will have something for you!"]
     ]
     div [ClassName "level"] [
       div [ClassName "level-item"] [
         a [
-          ClassName "notification is-info"
+          ClassName "notification is-primary"
           Href "https://www.eventbrite.es/e/fableconf-bordeaux-tickets-34089709238"
         ] [str "GET YOUR TICKET NOW!"]
+      ]
+    ]
+    h1 [ClassName "title is-1 has-text-centered"] [str "Sponsors"]
+    h4 [ClassName "subtitle is-4 has-text-centered"] [str "Many thanks to our fabulous sponsors who make this conference possible!"]
+    div [ClassName "level"] [
+      div [ClassName "level-item"] [
+        a [Href "http://fsharp.org/"] [
+          Image.image [] [img [Src "img/fsharp.png"]]
+        ]
       ]
     ]
   ]
--- src/Info/View.fs
+++ src/Info/View.fs
@@ -14,7 +14,7 @@ let root =
     Image.image [] [img [Src "img/cap-sciences.jpg"]]
     div [ClassName "content"] [
       br []
-      p [] [str "Bordeaux is a splendid city and a popular touristic destination. Make the most of your trip and relax during the weekend enojoying its restaurants, cafés and world-famous wineries, among many other attractions."]
+      p [] [str "Bordeaux is a splendid city and a popular touristic destination. Make the most of your trip and relax during the weekend enjoying its restaurants, cafés and world-famous wineries, among many other attractions."]
     ]
     iframe [
       Src "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5656.240080660091!2d-0.5584963664357877!3d44.85985097909837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd55288fe041f36f%3A0xc849cfd5c16a7bb8!2sCap+Sciences!5e0!3m2!1sen!2ses!4v1499327326158"
