acb590a26c0dd908cbacb9688de9b97c60b207b8
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-07-06 16:34:19 +0200

    Fix navbar buttons spacing

--- src/Navbar/View.fs
+++ src/Navbar/View.fs
@@ -2,32 +2,34 @@ module Navbar.View
 
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
+open Elmish.Bulma.Components
+open Elmish.Bulma.Elements.Form
 
 let navButton classy href faClass txt =
-  a
-    [ ClassName (sprintf "button %s" classy)
-      Href href ]
-    [ span
-        [ ClassName "icon" ]
-        [ i
-            [ ClassName (sprintf "fa %s" faClass) ]
-            [ ] ]
-      span
-        [ ]
-        [ str txt ] ]
+  Control.control [ ]
+    [ a [ ClassName (sprintf "button %s" classy)
+          Href href ]
+        [ span
+            [ ClassName "icon" ]
+            [ i
+                [ ClassName (sprintf "fa %s" faClass) ]
+                [ ] ]
+          span
+            [ ]
+            [ str txt ] ] ]
 
 let navButtons =
-  span
-    [ ClassName "nav-item" ]
-    [ navButton "twitter" "https://twitter.com/FableCompiler" "fa-twitter" "Twitter"
-      navButton "github" "https://gitter.im/fable-compiler/Fable" "fa-comments" "Gitter" ]
+  Nav.item [ ]
+    [ Field.field [ Field.isGroupedLeft ]
+        [ navButton "twitter" "https://twitter.com/FableCompiler" "fa-twitter" "Twitter"
+          navButton "github" "https://gitter.im/fable-compiler/Fable" "fa-comments" "Gitter" ] ]
 
 let root =
-  nav
-    [ ClassName "nav" ]
-    [ div
-        [ ClassName "nav-left" ]
+  Nav.nav
+    [ ]
+    [ Nav.left [ ]
         [ h1
             [ ClassName "nav-item is-brand title is-4" ]
             [ str "FableConf 2017" ] ]
-      navButtons ]
+      Nav.right [ ]
+        [ navButtons ] ]
