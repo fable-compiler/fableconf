f3a347476cf1dac3f4b0d952a553d0720636c3f9
francois nicaise <whitetigle@gmail.com>
2019-05-03 14:49:55 +0200

    added CFP + refactored to Fable.Core 3 family

--- src/Page/About.fs
+++ src/Page/About.fs
@@ -60,32 +60,34 @@ module About =
         
         ] [
           div [] [
-            div [
-              ClassName "ftitle"
-            ] [
-              Heading.h1 [] [
-                  span [ ClassName "top" ] [ str "FABLE" ]
-                  br []
-                  span [ ClassName "bottom" ] [ str "CONF'19" ]
-              ]
+            fade [] [
               div [
-                ClassName "dates"
+                ClassName "ftitle"
               ] [
-                  span [ ClassName "date"] [ str "6/7 sept 2019" ]
-                  br []
-                  span [ ClassName "date"] [ 
-                    strong [] [str "Antwerp" ]
-                    str ", Belgium" ]
-              ]
-              //hr []
-              div [ 
-                ClassName "axxesLogo"
-              ] [ 
-                span [] [ strong [] [ str "Powered by "]]
-                a [ Href "https://axxes.com/"; Target "_blank" ] [
-                  img [
-                    Src "./logo.png"
-                  ] 
+                Heading.h1 [] [
+                    span [ ClassName "top" ] [ str "FABLE" ]
+                    br []
+                    span [ ClassName "bottom" ] [ str "CONF'19" ]
+                ]
+                div [
+                  ClassName "dates"
+                ] [
+                    span [ ClassName "date"] [ str "6/7 sept 2019" ]
+                    br []
+                    span [ ClassName "date"] [ 
+                      strong [] [str "Antwerp" ]
+                      str ", Belgium" ]
+                ]
+                //hr []
+                div [ 
+                  ClassName "axxesLogo"
+                ] [ 
+                  span [] [ strong [] [ str "Powered by "]]
+                  a [ Href "https://axxes.com/"; Target "_blank" ] [
+                    img [
+                      Src "./logo.png"
+                    ] 
+                  ]
                 ]
               ]
             ]
@@ -101,20 +103,106 @@ module About =
         
         ] [
           Hero.body [ Props [ ClassName "addMargins"] ] [
-            Content.content [] [
-              p[] [strong [] [str "Come to the beautiful city of Antwerp and be part of the FABLECONF conferences! This year again FABLECONF will provide two days of F# and Javascript opportunities for learning and meeting people frome the Fable community." ]]
-              h2[ ] [str "Friday: talks"]
-              h4[ ] [str "Ankerrui 9, 2000 Antwerpen"]
-              p [][str "Day one will consists of talks. "; i[][ str "(More information to come soon...)"]]
-              h2[ ] [str "Saturday: workshops"]
-              h4[ ] [str "Entrepotkaai 10A, 2000 Antwerpen"]
-              p [][str "Day two will be filled with practical workshops and chances to sit down and works alongside members of the F# community to give confidence in writing Fable applications that take full advantage of F# and Javascript."]
-              h2[ ] [str "F# enlightenment"]
-              p[] [ str "No matter if you're new to Fable, don't have much experience in front or server development or even if you don't know F# yet. If you're a developer interested in writing code using a functional programming language designed for hogh productivity and with cutting-edge tooling, this year's FABLECONF will have something for you!"]
+            Columns.columns [] [
+              Column.column [ 
+                Column.Width (Screen.All, Column.IsThreeQuarters)
+              ] [
+                Content.content [] [
+                  p[] [strong [] [str "Come to the beautiful city of Antwerp and be part of the FABLECONF conferences! This year again FABLECONF will provide two days of F# and Javascript opportunities for learning and meeting people frome the Fable community." ]]
+                  h2[ ] [str "Friday: talks"]
+                  h4[ ] [str "Ankerrui 9, 2000 Antwerpen"]
+                  p [][str "Day one will consists of talks. "; i[][ str "(More information to come soon...)"]]
+                  h2[ ] [str "Saturday: workshops"]
+                  h4[ ] [str "Entrepotkaai 10A, 2000 Antwerpen"]
+                  p [][str "Day two will be filled with practical workshops and chances to sit down and works alongside members of the F# community to give confidence in writing Fable applications that take full advantage of F# and Javascript."]
+                  h2[ ] [str "F# enlightenment"]
+                  p[] [ str "No matter if you're new to Fable, don't have much experience in front or server development or even if you don't know F# yet. If you're a developer interested in writing code using a functional programming language designed for hogh productivity and with cutting-edge tooling, this year's FABLECONF will have something for you!"]
+                ]
+              ]
             ]
           ]
         ]
       
+    let cfp =  
+        Hero.hero [ 
+          Hero.IsMedium
+          Hero.Props [
+            ClassName "contents"
+          ]          
+        ] [
+          Hero.body [ Props [ ClassName "addMargins"] ] [
+            Content.content [] [
+              Columns.columns [ ] [
+                Column.column [
+                  Column.Width (Screen.All, Column.IsThreeQuarters)
+                ] [
+                  Content.content [] [
+                    h2[ ] [str "Call for papers"]
+                    p [] [
+                      str "Do you want to be part of one of the best events organized by the F# community? Have you been to the past editions in Bordeaux and Berlin and think it's time to live it from the other side? Then this is your opportunity to send a talk proposal for FableConf 2019 taking place the next September at the beautiful city of Antwerp in Belgium!"
+                    ]
+                    p [] [
+                      str "We're looking for talks and workshops about Fable, either introductory or advanced. But we're also open to broader topics like other uses of F# for web projects, or related technologies commonly used in Fable projects, like Webpack, JS optimization, CSS styling, etc. Really excited to know about all your great ideas to help the community!"
+                    ]
+                  ]
+                  a [ 
+                    ClassName "fbutton"
+                    Href "https://docs.google.com/forms/d/e/1FAIpQLSfarg6l9_SRju5ynIAA2OwrFRGuy2sUHUrd-T-P6q1SjVyDow/viewform"
+                    Target "_blank"
+                  ] [
+                    div [ClassName "innerLeft"] []
+                    div [ClassName "innerRight"] [ str "SUBMIT YOUR IDEA!"]              
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ]
+
+    let sponsors = 
+        Hero.hero [ 
+          Hero.IsLarge
+          Hero.Props [
+            ClassName "contents"
+          ]
+        
+        ] [
+          Hero.body [ Props [ ClassName "addMargins"] ] [
+            Content.content [] [
+              h2[ ] [str "Sponsors"]
+            ]
+            Tile.ancestor [] [
+              Tile.parent [
+                Tile.IsVertical
+                Tile.Size Tile.Is2
+                ] 
+                [
+                Tile.child [] [
+                  zoom [] [
+                    a [ Href "https://axxes.com/"; Target "_blank" ] [
+                      Image.image [ Image.Is128x128 ]
+                        [ img [ Src "./logo.png" ] ]
+                    ]
+                  ]
+                ]
+              ]
+              Tile.parent [
+                Tile.IsVertical
+              ] [
+                Tile.child [] [
+                  zoom [] [
+                    Image.image [ Image.Is96x96 ]
+                      [ 
+                        a [ Href "https://www.biensurgraphisme.com/about"; Target "_blank"] [
+                          img [ Src "https://static.wixstatic.com/media/df463e_ae881edc7926481fb32950bbd51745a0~mv2.png/v1/crop/x_0,y_30,w_537,h_619/fill/w_201,h_230,al_c,q_80,usm_0.66_1.00_0.01/df463e_ae881edc7926481fb32950bbd51745a0~mv2.webp" ] ]
+                        ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ]
+
     let coc  = 
         Hero.hero [ 
           Hero.IsLarge
@@ -128,7 +216,7 @@ module About =
               h2[ ] [str "Code of Conduct"]
               h4[ ] [str "Be responsible, be open and be considerate"]
             ]
-            Columns.columns [ Columns.IsMobile ] [
+            Columns.columns [ ] [
               Column.column [
                 Column.Width (Screen.All, Column.IsThreeQuarters)
               ] [
@@ -138,7 +226,11 @@ module About =
                   ]
                 ]
               ]
-              Column.column [] [
+              Column.column [
+                Column.Modifiers [
+                  Modifier.IsHiddenOnly(Screen.Mobile, true)
+                ]
+              ] [
                 zoom [] [
                   Image.image [ Image.Is128x128 ]
                     [ img [ Src "https://i2.wp.com/diversitycharter.org/wp-content/uploads/2016/05/sharelogo_small.png?w=200&ssl=1" ] ]
@@ -155,10 +247,6 @@ module About =
       Footer.footer[ Props [ Id "footer"] ][
         Level.level [
         ] [
-          Level.item [] [ 
-            a [ Href "#"] [ strong [] [str "CODE OF CONDUCT"]]
-          ]
-          cesure
           Level.item [] [ 
             span [] [ 
               str "web design by "
@@ -179,7 +267,9 @@ module About =
       div [] [
         cover
         fade [] [about]
-        pic
+        fade [] [pic]
+        fade [] [cfp]
+        fade [] [sponsors]
         fade [] [coc]
         footer
       ]
